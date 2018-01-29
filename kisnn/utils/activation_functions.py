522919f5a2bde8444069b9e0419c16b930c84687
Guilherme Perego <guilherme_perego@hotmail.com>
2018-01-29 17:46:17 -0200

    Sigmoid implemented, better testing

--- kisnn/utils/activation_functions.py
+++ kisnn/utils/activation_functions.py
@@ -1,8 +1,10 @@
 from enum import IntEnum
+from math import exp
 
 class ActivationType(IntEnum):
     NONE = 1
     SIGN = 2
+    SIGMOID = 3
 
 class ActivationFunctions:
     @staticmethod
@@ -11,6 +13,8 @@ class ActivationFunctions:
             return x
         if(type == ActivationType.SIGN):
             return ActivationFunctions.sign(x)
+        if(type == ActivationType.SIGMOID):
+            return ActivationFunctions.sigmoid(x)
         else:
             raise ValueError("Invalid Activation Type")
 
@@ -18,8 +22,21 @@ class ActivationFunctions:
     def sign(x):
         return 1 if x >= 0 else -1
 
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + exp(-x))
+
 # Testing
 if __name__ == "__main__":
-    print(str(ActivationFunctions.activate(-0.2, ActivationType.SIGN)))
-    print(str(ActivationFunctions.activate(0, ActivationType.SIGN)))
-    print(str(ActivationFunctions.activate(.222, ActivationType.SIGN-1)))
+    test = [0, 1.5, -.6]
+
+    print("Activation Functions Testing")
+    print()
+
+    for x in ActivationType:
+        print("Testing: " + str(x) + "\n")
+        for i in range(len(test)):
+            print("Test %d - Value: %.2f" % (i, test[i]))
+            print("Result: " + str(ActivationFunctions.activate(test[i], x)))
+            print()
+        print("------")
